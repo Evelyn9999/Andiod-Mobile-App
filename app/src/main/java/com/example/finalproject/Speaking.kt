d6ffaf656c5ae3fd5899fd4f62697a88aec9a3d7
Evelyn <sapphiry0404@gmail.com>
2024-04-30 06:55:47 +0300

    basic structure of speaking

--- app/src/main/java/com/example/finalproject/Speaking.kt
+++ app/src/main/java/com/example/finalproject/Speaking.kt
@@ -3,6 +3,7 @@ package com.example.finalproject
 import android.media.MediaRecorder
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,11 +12,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -26,18 +30,26 @@ import java.io.IOException
 
 @Composable
 fun <MediaPlayer> Speaking(navController: NavHostController) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-    ){
-        Text("Speaking", fontSize = 20.sp)
-        Spacer(modifier = Modifier
-            .fillMaxWidth()
-            .background(Color.Gray))
-        Button(onClick = { navController.navigate("HomePage") }) {
-            Text(text = "Back to HomePage", fontSize = 20.sp)
 
+    val image = painterResource(R.drawable.speaking)
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = image,
+            contentDescription = "Map of Finland",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.matchParentSize()
+        )
+
+        Button(
+            onClick = { navController.navigate("HomePage") },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0x8FFFEB3B),
+            ),
+        ) {
+            Text(text = "BACK", fontSize = 15.sp, color = Color.DarkGray)
         }
+
     }
 }
 

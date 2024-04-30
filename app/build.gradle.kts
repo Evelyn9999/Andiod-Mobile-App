2ec62befbdd66ffb79b47108d87289841ee067f2
Evelyn <sapphiry0404@gmail.com>
2024-04-30 08:52:28 +0300

    re-iniatiate the more info page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,6 @@ dependencies {
 
     implementation( "androidx.compose.material:material-icons-core:<version>")
     implementation( "androidx.compose.material:material-icons-extended:<version>")
-    implementation( " org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0 ")
-    implementation( "org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+    implementation ("io.ktor:ktor-client-android:1.6.7")
+    implementation ("io.ktor:ktor-client-serialization:1.6.7")
 }
--- app/src/main/java/com/example/finalproject/MoreInfo.kt
+++ app/src/main/java/com/example/finalproject/MoreInfo.kt
@@ -1,36 +1,19 @@
 package com.example.finalproject
 
-import androidx.compose.foundation.background
 import androidx.compose.runtime.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import kotlinx.coroutines.launch
-import java.net.HttpURLConnection
-import java.net.URL
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import kotlinx.serialization.*
-import kotlinx.serialization.json.*
+
 
 
 @Composable
 fun MoreInfo(navController: NavHostController) {
-    val scope = rememberCoroutineScope()
-    var apiData by remember { mutableStateOf("Loading data...") }
-
-    LaunchedEffect(Unit) {
-        scope.launch {
-            apiData = fetchApiData()
-        }
-    }
-
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -38,25 +21,13 @@ fun MoreInfo(navController: NavHostController) {
     ){
         Text("More about Finland...", fontSize = 25.sp)
         Spacer(modifier = Modifier.height(16.dp))
-        Text(apiData, fontSize = 18.sp) // Displaying the API data
-        Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = { navController.navigate("HomePage") }) {
             Text(text = "Back", fontSize = 20.sp)
         }
     }
 }
 
-suspend fun fetchApiData(): String = withContext(Dispatchers.IO) {
-    try {
-        val url = URL("https://pxdata.stat.fi/PXWeb/api/v1/en/StatFin")
-        with(url.openConnection() as HttpURLConnection) {
-            requestMethod = "GET"
-            return@withContext inputStream.bufferedReader().readText()
-        }
-    } catch (e: Exception) {
-        return@withContext "Failed to load data: ${e.message}"
-    }
-}
+
 
 @Preview(showBackground = true)
 @Composable
